1127abb4eec5d999428130eda4dedbaa9a039c5a
maSemeao <maurilio.ssemeao@gmail.com>
2023-01-22 19:05:46 -0300

    feat:  add props to Pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,3 +1,11 @@
-export const Pokemon = () => {
-  return <h2>Nome Do Pokemon: Pikachu</h2>;
+export const Pokemon = ({ name = 'Pikachu' }) => {
+  return <h2>Nome Do Pokemon: {name}</h2>;
 };
+
+// import React from 'react';
+
+// export class Pokemon extends React.Component {
+//   render() {
+//     return <h2> Nome Do Pokemon: Pikachu</h2>;
+//   }
+// }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,11 @@ export const Home = () => {
   return (
     <div>
       <h1>Meu site Pokemon</h1>
-      <Pokemon />
+      <Pokemon name="Pikachu" />
+      <Pokemon name="Sharizard" />
+      <Pokemon name="Squirtle" />
+      <Pokemon name="Charmander" />
+      <Pokemon name="Dito" />
     </div>
   );
 };
